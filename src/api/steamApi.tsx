66a073290ab1b872006bb872afe8f3035adceec9
aurilaus <aurilaus@gmail.com>
2023-12-24 17:59:04 +0200

    SteamProfile

--- src/api/steamApi.tsx
+++ src/api/steamApi.tsx
@@ -1,12 +1,11 @@
 import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 import {steamURL} from "../utils/constants.tsx";
-import {ISteamProfile} from "../utils/types.ts";
 
 export const steamApi = createApi({
     reducerPath: 'steamApi',
     baseQuery: fetchBaseQuery({ baseUrl: steamURL, mode: "no-cors" }),
     endpoints: (builder) => ({
-        getSteamProfile: builder.query<ISteamProfile, string>({
+        getSteamProfile: builder.query({
             query: (steamId) => `ISteamUser/GetPlayerSummaries/v0002/?key=7F73B61B2C14C0796498A5E2EBC345F4&steamids=${steamId}`,
         }),
     }),
