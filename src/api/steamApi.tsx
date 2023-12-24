3cf6bb37c81e6ed23973d08fcf6eafe4f1052ad3
aurilaus <aurilaus@gmail.com>
2023-12-24 11:35:44 +0200

    SteamProfile

--- src/api/steamApi.tsx
+++ src/api/steamApi.tsx
@@ -7,7 +7,7 @@ export const steamApi = createApi({
     baseQuery: fetchBaseQuery({ baseUrl: steamURL }),
     endpoints: (builder) => ({
         getSteamProfile: builder.query<ISteamProfile, string>({
-            query: (steamId) => `ISteamUser/GetPlayerSummaries/v0002/?key=80B5B81001256250B648EA670A2AEE1A&steamids=${steamId}`,
+            query: (steamId) => `ISteamUser/GetPlayerSummaries/v0002/?key=EFA9F0B687F53BB0093EAAFF8D359778&steamids=${steamId}`,
         }),
     }),
 })
