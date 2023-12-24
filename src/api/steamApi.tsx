3be734d3dd22c3eb1aa3762fc8f0aa23f7e3e05f
aurilaus <aurilaus@gmail.com>
2023-12-24 17:35:44 +0200

    SteamProfile

--- src/api/steamApi.tsx
+++ src/api/steamApi.tsx
@@ -4,7 +4,7 @@ import {ISteamProfile} from "../utils/types.ts";
 
 export const steamApi = createApi({
     reducerPath: 'steamApi',
-    baseQuery: fetchBaseQuery({ baseUrl: steamURL }),
+    baseQuery: fetchBaseQuery({ baseUrl: steamURL, mode: "no-cors" }),
     endpoints: (builder) => ({
         getSteamProfile: builder.query<ISteamProfile, string>({
             query: (steamId) => `ISteamUser/GetPlayerSummaries/v0002/?key=7F73B61B2C14C0796498A5E2EBC345F4&steamids=${steamId}`,
