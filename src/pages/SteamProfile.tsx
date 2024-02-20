84faa0e710c34f87a7cc92301f0f566072f22795
aurilaus <akhor@nordpage.ru>
2024-02-20 17:50:43 +0200

    Test

--- src/pages/SteamProfile.tsx
+++ src/pages/SteamProfile.tsx
@@ -1,18 +1,6 @@
-import {useGetFriendsQuery, useGetSteamProfileQuery} from "../api/steamApi.tsx";
-import {InputText} from "primereact/inputtext";
-import  {useState} from "react";
-import {Button} from "primereact/button";
 import '../App.css';
-import {Status} from "../components/Status.tsx";
-import {ProgressSpinner} from "primereact/progressspinner";
-
 import 'primeicons/primeicons.css';
-import WebApp from "@twa-dev/sdk";
-import {IFriend} from "../utils/types.ts";
-import {Friend} from "../components/Friend.tsx";
-import {Toggle} from "../components/Toggle.tsx";
 import {Mocks} from "./Mocks.tsx";
-
 export function SteamProfile() {
  //   const steamId = localStorage.getItem("steamId")
 //     const [id, setId] = useState("")
